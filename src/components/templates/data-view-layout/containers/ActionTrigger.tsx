4586f50f7e0ce39c8d7020be6f206a161fb90e74
dangdinhtai0001 <dangdinhtai0001@gmail.com>
2025-04-22 23:05:13 +0700

    feat: update ActionTrigger to support dialog type

--- src/components/templates/data-view-layout/containers/ActionTrigger.tsx
+++ src/components/templates/data-view-layout/containers/ActionTrigger.tsx
@@ -1,38 +1,54 @@
 import { Button } from "@/components/atoms/button";
 import { ActionTriggerProps } from "../types";
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/atoms/dialog";
 
-function ActionTrigger({ type }: ActionTriggerProps) {
+function ActionTrigger({ type, label }: ActionTriggerProps) {
+  const renderButton = () => {
+    return <Button>{label}</Button>;
+  };
+  const renderModal = () => {
+    return (
+      <>
+        <Dialog>
+          <DialogTrigger asChild>{renderButton()}</DialogTrigger>
+          <DialogContent>
+            <DialogHeader>
+              <DialogTitle>{label}</DialogTitle>
+              <DialogDescription>This is a description</DialogDescription>
+            </DialogHeader>
+            <div>This is form </div>
+            <DialogFooter>
+              <Button variant="outline">Cancel</Button>
+              <Button>Submit</Button>
+            </DialogFooter>
+          </DialogContent>
+        </Dialog>
+      </>
+    );
+  };
+  const renderCustom = () => {
+    return <div>ActionTrigger custom</div>;
+  };
 
-    const renderButton = () => {
-        return (
-            <div data-grid={{ x: 0, y: 0, w: 12, h: 7 }}>
-                <Button>Button</Button>
-            </div>
-        )
-    }
-    const renderModal = () => {
-        return (
-            <div data-grid={{ x: 0, y: 1, w: 12, h: 7 }}>ActionTrigger modal</div>
-        )
-    }
-    const renderCustom = () => {
-        return (
-            <div data-grid={{ x: 0, y: 2, w: 12, h: 7 }}>ActionTrigger custom</div>
-        )
-    }
-
-    switch (type) {
-        case "button":
-            return renderButton();
-        case "modal":
-            return renderModal();
-        case "custom":
-            return renderCustom();
-        default:
-            return null;
-    }
+  switch (type) {
+    case "button":
+      return renderButton();
+    case "dialog":
+      return renderModal();
+    case "custom":
+      return renderCustom();
+    default:
+      return null;
+  }
 }
 
 ActionTrigger.displayName = "ActionTrigger";
 export default ActionTrigger;
-
