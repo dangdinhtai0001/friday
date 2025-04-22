e43edfa804492511978cd1333ea2a25e0cae20b9
dangdinhtai0001 <dangdinhtai0001@gmail.com>
2025-04-22 23:05:02 +0700

    feat: add new Dialog components (Dialog, DialogContent, etc.)

---
+++ src/components/atoms/dialog/DialogFooter.tsx
@@ -0,0 +1,17 @@
+import { cn } from "@/composables/lib/utils";
+
+export function DialogFooter({
+  className,
+  ...props
+}: React.ComponentProps<"div">) {
+  return (
+    <div
+      data-slot="dialog-footer"
+      className={cn(
+        "flex flex-col-reverse gap-2 sm:flex-row sm:justify-end",
+        className
+      )}
+      {...props}
+    />
+  );
+}
---
+++ src/components/atoms/dialog/DialogHeader.tsx
@@ -0,0 +1,14 @@
+import { cn } from "@/composables/lib/utils";
+
+export function DialogHeader({
+  className,
+  ...props
+}: React.ComponentProps<"div">) {
+  return (
+    <div
+      data-slot="dialog-header"
+      className={cn("flex flex-col gap-2 text-center sm:text-left", className)}
+      {...props}
+    />
+  );
+}
