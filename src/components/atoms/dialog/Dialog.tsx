e9a6463f1be803f8c8ea0a87c21749edfd7649d8
dangdinhtai <dangdinhtai0001@gmail.com>
2025-04-23 18:39:31 +0700

    feat: integrate Prettier and TailwindCSS plugin

--- src/components/atoms/dialog/Dialog.tsx
+++
@@ -1,8 +0,0 @@
-import * as React from "react";
-import * as DialogPrimitive from "@radix-ui/react-dialog";
-
-export function Dialog({
-  ...props
-}: React.ComponentProps<typeof DialogPrimitive.Root>) {
-  return <DialogPrimitive.Root data-slot="dialog" {...props} />;
-}
