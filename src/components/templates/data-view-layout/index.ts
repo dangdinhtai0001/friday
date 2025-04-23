d34d2874058d7a7b00816926da4ef08ac3ffd74b
dangdinhtai <dangdinhtai0001@gmail.com>
2025-04-23 18:40:05 +0700

    chore: update export paths for new trigger components

--- src/components/templates/data-view-layout/index.ts
+++ src/components/templates/data-view-layout/index.ts
@@ -1,3 +1,8 @@
 export { default as DataViewLayout } from "./DataViewLayoutProvider";
 export { default as FilterContainer } from "./containers/FilterContainer";
+
 export { default as ActionContainer } from "./containers/ActionContainer";
+export { default as ButtonTrigger } from "./containers/ButtonTrigger";
+export { default as CustomTrigger } from "./containers/CustomTrigger";
+export { default as DialogTrigger } from "./containers/DialogTrigger";
+export { default as FormTrigger } from "./containers/FormTrigger";
