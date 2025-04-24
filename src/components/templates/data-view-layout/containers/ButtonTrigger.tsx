fc6c6b87b4e9baf3311b33664fe5ff1c189d153c
dangdinhtai <dangdinhtai0001@gmail.com>
2025-04-24 11:30:10 +0700

    feat: update ButtonTrigger to support loading indicators

--- src/components/templates/data-view-layout/containers/ButtonTrigger.tsx
+++ src/components/templates/data-view-layout/containers/ButtonTrigger.tsx
@@ -3,6 +3,8 @@ import { ButtonTriggerProps } from "../types";
 import { EventBusInstance } from "@/composables/lib/EventBus";
 import { resolveEventName } from "../Utils";
 import { useDataViewContext } from "../context/DataViewContext";
+import { useState } from "react";
+import { Spinner } from "@/components/atoms/loader";
 
 function ButtonTrigger({
   label,
@@ -12,12 +14,16 @@ function ButtonTrigger({
 }: ButtonTriggerProps) {
   const { state } = useDataViewContext();
 
+  const [isLoading, setIsLoading] = useState(false);
+
   const handleTrigger = () => {
-    EventBusInstance.emit(resolveEventName(eventName || "", state.id), {});
+    EventBusInstance.emit(resolveEventName(eventName || "", state.id), { setIsLoading });
   };
   return (
-    <Button className={className} variant={variant} onClick={handleTrigger}>
-      {label}
+    <Button className={className} variant={variant} onClick={handleTrigger} disabled={isLoading}>
+      <div className="flex items-center gap-2">
+        {isLoading ? <Spinner className="w-12 h-12" /> : null} {label}
+      </div>
     </Button>
   );
 }
