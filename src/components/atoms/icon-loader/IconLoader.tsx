b591c7d8f6632ec012f96f6ede153e1ee304d110
dangdinhtai0001 <dangdinhtai0001@gmail.com>
2025-04-28 01:43:31 +0700

    feat(icon-loader): add IconLoader component for dynamic SVG loading

---
+++ src/components/atoms/icon-loader/IconLoader.tsx
@@ -0,0 +1,40 @@
+import { cn } from "@/composables/lib/utils";
+
+const allIconFiles = import.meta.glob("@/assets/icons/*.svg", {
+  query: "?raw",
+  import: "default",
+  eager: true,
+});
+
+// Chuyển đổi object trả về thành dạng dễ sử dụng hơn
+const icons: { [key: string]: string } = {};
+for (const [path, content] of Object.entries(allIconFiles)) {
+  // Lấy tên file từ đường dẫn
+  const fileName = path.split("/").pop()?.replace(".svg", "") || "";
+  if (fileName) {
+    icons[fileName] = content as string;
+  }
+}
+
+interface IconLoaderProps {
+  name: string;
+  className?: string;
+}
+
+const IconLoader = ({ name, className }: IconLoaderProps) => {
+  const iconContent = icons[name];
+
+  if (!iconContent) {
+    console.warn(`SVG file with name "${name}" not found.`);
+    return null;
+  }
+
+  return (
+    <div
+      className={cn("h-4 w-4", className)}
+      dangerouslySetInnerHTML={{ __html: iconContent }}
+    />
+  );
+};
+
+export default IconLoader;
