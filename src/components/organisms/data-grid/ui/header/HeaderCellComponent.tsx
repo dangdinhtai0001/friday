870e3e2553a0be1e79eeefab39b9f202f8b76d16
dangdinhtai0001 <dangdinhtai0001@gmail.com>
2025-05-02 01:32:36 +0700

    feat(i18n): add internationalization for DataGrid and MultiSelect

--- src/components/organisms/data-grid/ui/header/HeaderCellComponent.tsx
+++ src/components/organisms/data-grid/ui/header/HeaderCellComponent.tsx
@@ -1,13 +1,91 @@
-export interface HeaderCellComponentProps {
-    children: React.ReactNode;
+import React from "react";
+import { Button } from "@/components/atoms/button";
+import { IconLoader } from "@/components/atoms/icon-loader";
+import {
+  DropdownMenu,
+  DropdownMenuTrigger,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuSeparator,
+  DropdownMenuSub,
+  DropdownMenuSubTrigger,
+  DropdownMenuPortal,
+  DropdownMenuSubContent,
+  DropdownMenuCheckboxItem,
+} from "@/components/organisms/dropdown-menu";
+import { DropdownMenuGroup } from "@radix-ui/react-dropdown-menu";
+import translate from "@/composables/lib/international";
+import { HeaderContext } from "@tanstack/react-table";
+
+export interface HeaderCellComponentProps<TData> {
+  children?: React.ReactNode;
+  headerContext: HeaderContext<TData, unknown>;
 }
 
-function HeaderCellComponent({ children }: HeaderCellComponentProps) {
-    return (
-        <div className="header-cell-component ">
-            {children}
+function HeaderCellComponent<TData>({
+  children,
+}: HeaderCellComponentProps<TData>) {
+  return (
+    <>
+      <div className="header-cell-comp flex items-center justify-between">
+        <div className="header-cell-comp-display">{children}</div>
+        <div className="header-cell-comp-menu flex items-center">
+          <DropdownMenu>
+            <DropdownMenuTrigger asChild>
+              <Button variant="borderless" size="icon">
+                <IconLoader name="dots-vertical" className="text-black-40" />
+              </Button>
+            </DropdownMenuTrigger>
+            <DropdownMenuContent className="" align="start" side="bottom">
+              <DropdownMenuGroup>
+                <DropdownMenuItem className="flex items-center gap-2">
+                  <IconLoader name="arrow-narrow-up" className="size-16" />
+                  {translate("component.data-grid.header.menu.sort-ascending")}
+                </DropdownMenuItem>
+                <DropdownMenuItem className="flex items-center gap-2">
+                  <IconLoader name="arrow-narrow-down" className="size-16" />
+                  {translate("component.data-grid.header.menu.sort-descending")}
+                </DropdownMenuItem>
+              </DropdownMenuGroup>
+              <DropdownMenuSeparator />
+              <DropdownMenuGroup>
+                <DropdownMenuSub>
+                  <DropdownMenuSubTrigger className="flex items-center gap-2">
+                    <IconLoader name="pin" className="size-16" />
+                    {translate("component.data-grid.header.menu.pin-column")}
+                  </DropdownMenuSubTrigger>
+                  <DropdownMenuPortal>
+                    <DropdownMenuSubContent>
+                      <DropdownMenuCheckboxItem checked={true}>
+                        {translate("component.data-grid.header.menu.no-pin")}
+                      </DropdownMenuCheckboxItem>
+                      <DropdownMenuCheckboxItem>
+                        {translate("component.data-grid.header.menu.pin-left")}
+                      </DropdownMenuCheckboxItem>
+                      <DropdownMenuCheckboxItem>
+                        {translate("component.data-grid.header.menu.pin-right")}
+                      </DropdownMenuCheckboxItem>
+                    </DropdownMenuSubContent>
+                  </DropdownMenuPortal>
+                </DropdownMenuSub>
+              </DropdownMenuGroup>
+              <DropdownMenuSeparator />
+              <DropdownMenuGroup>
+                <DropdownMenuItem className="flex items-center gap-2">
+                  <IconLoader name="table-column" className="size-16" />
+                  {translate("component.data-grid.header.menu.choose-column")}
+                </DropdownMenuItem>
+                <DropdownMenuItem className="flex items-center gap-2">
+                  <IconLoader name="refresh" className="size-16" />
+                  {translate("component.data-grid.header.menu.reset-column")}
+                </DropdownMenuItem>
+              </DropdownMenuGroup>
+            </DropdownMenuContent>
+          </DropdownMenu>
         </div>
-    );
+      </div>
+    </>
+  );
 }
 
 export default HeaderCellComponent;
---
+++ src/composables/lib/international.ts
@@ -0,0 +1,46 @@
+// international.ts
+
+import { get } from "lodash-es"; // Import Lodash
+
+// Define the structure of the dictionary object
+type Dictionary = {
+  [key: string]: string | Dictionary; // Each key can be a string or another nested dictionary
+};
+
+const dictionary: Dictionary = {
+  vi: {
+    component: {
+      "multi-select": {
+        "search-placeholder": "Tìm kiếm",
+        "result-placeholder": "Lựa chọn",
+        "no-results": "Không tìm thấy kết quả",
+        "select-all": "Chọn tất cả",
+      },
+      "data-grid": {
+        header: {
+          menu: {
+            "sort-ascending": "Sắp xếp tăng dần",
+            "sort-descending": "Sắp xếp giảm dần",
+            "pin-column": "Ghim cột",
+            "pin-left": "Ghim cột trái",
+            "pin-right": "Ghim cột phải",
+            "no-pin": "Không ghim",
+            "choose-column": "Chọn cột",
+            "reset-column": "Reset cột",
+          },
+        },
+      },
+    },
+  },
+};
+
+// Define the type for the translate function
+type TranslateFunction = (key: string, lang?: string) => string;
+
+// Implement the translate function using Lodash's _.get
+const translate: TranslateFunction = (key, lang = "vi") => {
+  const result = get(dictionary, `${lang}.${key}`, key); // Use _.get to traverse the nested structure
+  return typeof result === "string" ? result : key; // Ensure the result is a string
+};
+
+export default translate;
