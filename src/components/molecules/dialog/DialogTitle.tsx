de26e5446af8b7eaee3f8439be599ebe2a78a842
dangdinhtai0001 <dangdinhtai0001@gmail.com>
2025-05-04 18:12:24 +0700

    refactor(dialog): Break down dialog into smaller subcomponents

---
+++ src/components/molecules/dialog/DialogTitle.tsx
@@ -0,0 +1,22 @@
+import * as React from "react";
+import * as DialogPrimitive from "@radix-ui/react-dialog";
+import { cn } from "@/composables/lib/utils";
+
+const DialogTitle = React.forwardRef<
+  React.ComponentRef<typeof DialogPrimitive.Title>,
+  React.ComponentPropsWithoutRef<typeof DialogPrimitive.Title>
+>(({ className, ...props }, ref) => (
+  <DialogPrimitive.Title
+    ref={ref}
+    className={cn(
+      // "text-lg font-semibold leading-none tracking-tight",
+      "typography-semibold-24 text-center",
+      className
+    )}
+    {...props}
+  />
+));
+
+DialogTitle.displayName = DialogPrimitive.Title.displayName;
+
+export { DialogTitle };
