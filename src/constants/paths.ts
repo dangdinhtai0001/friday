b54b2e08fd114666567637cc198cfc33a1bce3e2
dangdinhtai0001 <dangdinhtai0001@gmail.com>
2025-05-08 01:26:34 +0700

    feat: define application route paths in constants

---
+++ src/constants/paths.ts
@@ -0,0 +1,6 @@
+export const PATH_HOME = '/';
+export const PATH_LOGIN = '/login';
+export const PATH_UNAUTHORIZED = '/unauthorized';
+export const PATH_PROFILE = '/profile';
+export const PATH_ADMIN = '/admin';
+export const PATH_ADMIN_USERS = '/admin/users';
