dc2717cee040b1f139a899f39e96a1d290082785
dangdinhtai0001 <dangdinhtai0001@gmail.com>
2025-05-08 01:25:03 +0700

    Initial project setup with React, TypeScript, and Vite

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+import "@/assets/styles/main.css";
+import { RouterProvider } from "react-router";
+import { router } from "@/router";
+
+function App() {
+  return <RouterProvider router={router} />;
+}
+
+export default App;
