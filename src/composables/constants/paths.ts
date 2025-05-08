11766870d3f31db81f3bdea6848674d2756ff3c0
dangdinhtai <dangdinhtai0001@gmail.com>
2025-05-08 18:45:47 +0700

    feat: add route definitions for application pages

---
+++ src/composables/constants/paths.ts
@@ -0,0 +1,6 @@
+export const PATH_HOME = '/';
+export const PATH_LOGIN = '/login';
+export const PATH_LOGOUT = '/logout';
+export const PATH_UNAUTHORIZED = '/unauthorized';
+export const PATH_PROFILE = '/profile';
+export const PATH_COMPONENT = '/components';
