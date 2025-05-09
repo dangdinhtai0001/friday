8ff48eb42a806a08ddfdbb048fef29945213df9f
dangdinhtai <dangdinhtai0001@gmail.com>
2025-05-09 18:38:49 +0700

    feat(button): Add DynamicButton component with icon support and loading state

--- src/components/atoms/button/index.tsx
+++ src/components/atoms/button/index.tsx
@@ -1,4 +1,4 @@
 export { default as Button } from "./Button";
 export type { ButtonProps } from "./Button";
-export { default as DynamicButton } from "./IconButton";
+export { default as DynamicButton } from "./DynamicButton";
 export { buttonVariants } from "./variants";
