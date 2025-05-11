e1bf7d874248a3e92a77e316e829df5aaf9cd6d7
dangdinhtai0001 <dangdinhtai0001@gmail.com>
2025-05-12 00:57:29 +0700

    refactor: simplify AppLayout by removing navigation indicators

--- src/layouts/AppLayout.tsx
+++ src/layouts/AppLayout.tsx
@@ -1,17 +1,11 @@
-import { Outlet, useLocation, useNavigation } from "react-router";
+import { Outlet } from "react-router";
 import useRequireAuth from "@/composables/hooks/useRequireAuth";
 
 function AppLayout() {
   useRequireAuth();
-  const location = useLocation();
-  const navigation = useNavigation();
-  const isNavigating = Boolean(navigation.location);
 
   return (
     <>
-      <div>main layout nè</div>
-      {isNavigating && <div>Đang navigating nè ...</div>}
-      <div>location: {location.pathname}</div>
       <Outlet />
     </>
   );
