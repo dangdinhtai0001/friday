5ff1f9185a15ac48848270c6bde94f15c65b981d
dangdinhtai <dangdinhtai0001@gmail.com>
2025-05-12 18:38:43 +0700

    feat: export core sidebar components

---
+++ src/components/templates/sidebar/index.ts
@@ -0,0 +1,5 @@
+export { default as MCSidebar } from "./core/Sidebar";
+export { default as MCSidebarContent } from "./core/SidebarContent";
+export { default as MCSidebarRoot } from "./core/SidebarRoot";
+export { default as MCSidebarInset } from "./core/SidebarInset";
+export { default as MCSidebarTrigger } from "./toggle/SidebarTrigger";
