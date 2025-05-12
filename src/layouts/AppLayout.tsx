2b76a83d7e22f679f42aa7bb8b210828eb6b1675
dangdinhtai <dangdinhtai0001@gmail.com>
2025-05-12 18:37:49 +0700

    feat: integrate Sidebar components into AppLayout

--- src/layouts/AppLayout.tsx
+++ src/layouts/AppLayout.tsx
@@ -1,12 +1,38 @@
 import { Outlet } from "react-router";
 import useRequireAuth from "@/composables/hooks/useRequireAuth";
+import {
+  MCSidebar,
+  MCSidebarContent,
+  MCSidebarInset,
+  MCSidebarRoot,
+  MCSidebarTrigger,
+} from "@/components/templates/sidebar";
+import { cn } from "@/composables/utils/shadcn";
 
 function AppLayout() {
   useRequireAuth();
 
   return (
     <>
-      <Outlet />
+      <MCSidebarRoot defaultOpen={false}>
+        <MCSidebar>
+          <MCSidebarContent>
+            <div className="p-4">
+              <h3>Admin Panel</h3>
+            </div>
+          </MCSidebarContent>
+        </MCSidebar>
+        <MCSidebarInset>
+          <div
+            className={cn(
+              "border-black-5 flex h-[68px] items-center border-b-[0.5px]",
+            )}
+          >
+            <MCSidebarTrigger className="w-fit" />
+          </div>
+          <Outlet />
+        </MCSidebarInset>
+      </MCSidebarRoot>
     </>
   );
 }
