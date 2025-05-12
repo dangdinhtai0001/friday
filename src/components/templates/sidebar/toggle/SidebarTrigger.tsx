6f9c9e7584f014131aff4bad6f4ff528ef9a99b9
dangdinhtai <dangdinhtai0001@gmail.com>
2025-05-12 18:37:40 +0700

    feat: add SidebarTrigger component for toggle functionality

---
+++ src/components/templates/sidebar/toggle/SidebarTrigger.tsx
@@ -0,0 +1,44 @@
+import { Button } from "@/components/atoms/button";
+import { useSidebarContext } from "../context/SidebarContext";
+import { cn } from "@/composables/utils/shadcn";
+import { IconLoader } from "@/components/atoms/icon-loader";
+import { motion } from "motion/react";
+import { SIDEBAR_TRANSITION_DURATION } from "../constants";
+
+const iconTransition = { duration: SIDEBAR_TRANSITION_DURATION, type: "spring", stiffness: 200, damping: 20 };
+
+const rotationVariants = {
+  open: { rotate: 180 },
+  closed: { rotate: 0 },
+};
+
+function SidebarTrigger({
+  className,
+  onClick,
+  ...props
+}: React.ComponentProps<typeof Button>) {
+  const { state, actions } = useSidebarContext();
+
+  return (
+    <Button
+      data-sidebar="trigger"
+      data-slot="sidebar-trigger"
+      className={cn("", className)}
+      onClick={(event) => {
+        onClick?.(event);
+        actions.toggleSidebar();
+      }}
+      {...props}
+    >
+      <motion.div
+        variants={rotationVariants}
+        animate={state.open ? "open" : "closed"}
+        transition={iconTransition}
+      >
+        <IconLoader name="layout-sidebar-right-collapse" />
+      </motion.div>
+    </Button>
+  );
+}
+
+export default SidebarTrigger;
