806da5fa90b882ae488c918f8bbe17454451ff2d
dangdinhtai <dangdinhtai0001@gmail.com>
2025-05-13 15:43:24 +0700

    refactor: update sidebar component exports and add SidebarHeader/Footer

--- src/components/templates/sidebar/index.ts
+++ src/components/templates/sidebar/index.ts
@@ -1,5 +1,8 @@
 export { default as MCSidebar } from "./core/Sidebar";
-export { default as MCSidebarContent } from "./core/SidebarContent";
+export { default as MCSidebarContent } from "./container/SidebarContent";
 export { default as MCSidebarRoot } from "./core/SidebarRoot";
 export { default as MCSidebarInset } from "./core/SidebarInset";
 export { default as MCSidebarTrigger } from "./toggle/SidebarTrigger";
+export { default as MCSidebarHeader } from "./container/SidebarHeader";
+export { default as MCSidebarFooter } from "./container/SidebarFooter";
+
