aa7daff0d63aff202a4dd241a96f5908c90ebe2d
dangdinhtai <dangdinhtai0001@gmail.com>
2025-05-19 18:46:39 +0700

    feat: add @radix-ui/react-dropdown-menu dependency

--- src/components/organisms/tooltip/index.ts
+++
@@ -1,5 +0,0 @@
-export { default as TooltipProvider } from "./TooltipProvider";
-export { default as Tooltip } from "./Tooltip";
-export { default as TooltipTrigger } from "./TooltipTrigger";
-export { default as TooltipContent } from "./TooltipContent";
-
