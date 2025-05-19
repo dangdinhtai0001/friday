35bf4f6419528d2122b6ed44b56599fef569a47d
dangdinhtai0001 <dangdinhtai0001@gmail.com>
2025-05-20 01:29:27 +0700

    feat: add sidebar content components (Group, GroupAction, GroupContent, GroupLabel)

---
+++ src/components/organisms/sidebar/content/SidebarGroupContent.tsx
@@ -0,0 +1,17 @@
+import { cn } from "@/composables/utils/shadcn";
+
+function SidebarGroupContent({
+  className,
+  ...props
+}: React.ComponentProps<"div">) {
+  return (
+    <div
+      data-slot="sidebar-group-content"
+      data-sidebar="group-content"
+      className={cn("w-full", className)}
+      {...props}
+    />
+  );
+}
+
+export default SidebarGroupContent;
