d9fc33ced4ef7db712914bfc5d2d3f7a1d0a6b02
dangdinhtai <dangdinhtai0001@gmail.com>
2025-05-19 18:47:00 +0700

    feat: add sidebar menu components

---
+++ src/components/templates/sidebar/menu/SidebarMenu.tsx
@@ -0,0 +1,14 @@
+import { cn } from "@/composables/utils/shadcn";
+
+function SidebarMenu({ className, ...props }: React.ComponentProps<"ul">) {
+  return (
+    <ul
+      data-slot="sidebar-menu"
+      data-sidebar="menu"
+      className={cn("flex w-full min-w-0 flex-col gap-1", className)}
+      {...props}
+    />
+  );
+}
+
+export default SidebarMenu;
---
+++ src/components/templates/sidebar/menu/SidebarMenuItem.tsx
@@ -0,0 +1,14 @@
+import { cn } from "@/composables/utils/shadcn";
+
+function SidebarMenuItem({ className, ...props }: React.ComponentProps<"li">) {
+  return (
+    <li
+      data-slot="sidebar-menu-item"
+      data-sidebar="menu-item"
+      className={cn("group/menu-item relative", className)}
+      {...props}
+    />
+  );
+}
+
+export default SidebarMenuItem;
