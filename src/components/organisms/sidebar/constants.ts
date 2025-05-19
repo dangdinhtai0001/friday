16a511c5722b7f42d9fc87696e28b413e860fef1
dangdinhtai0001 <dangdinhtai0001@gmail.com>
2025-05-20 01:29:10 +0700

    feat: add sidebar constants and type definitions

---
+++ src/components/organisms/sidebar/constants.ts
@@ -0,0 +1,6 @@
+export const SIDEBAR_EXPANDED_WIDTH = "212px";
+export const SIDEBAR_COLLAPSED_WIDTH = "80px";
+export const SIDEBAR_TRANSITION_DURATION = "0.3";
+export const SIDEBAR_HEADER_HEIGHT = "68px";
+
+// expandedWidth, collapsedWidth
