d519b182d0bcbd6227be70c220c428ab1582d09f
dangdinhtai0001 <dangdinhtai0001@gmail.com>
2025-05-20 01:29:49 +0700

    feat: add index file for sidebar exports

---
+++ src/components/organisms/sidebar/index.ts
@@ -0,0 +1,16 @@
+export { default as MCSidebar } from "./core/Sidebar";
+export { default as MCSidebarContent } from "./container/SidebarContent";
+export { default as MCSidebarRoot } from "./core/SidebarRoot";
+export { default as MCSidebarInset } from "./core/SidebarInset";
+export { default as MCSidebarTrigger } from "./toggle/SidebarTrigger";
+export { default as MCSidebarHeader } from "./container/SidebarHeader";
+export { default as MCSidebarFooter } from "./container/SidebarFooter";
+
+export { default as MCSidebarGroup } from "./content/SidebarGroup";
+export { default as MCSidebarGroupLabel } from "./content/SidebarGroupLabel";
+export { default as MCSidebarGroupAction } from "./content/SidebarGroupAction";
+export { default as MCSidebarGroupContent } from "./content/SidebarGroupContent";
+
+export { default as MCSidebarMenu } from "./menu/SidebarMenu";
+export { default as MCSidebarMenuItem } from "./menu/SidebarMenuItem";
+export { default as MCSidebarMenuButton } from "./menu/SidebarMenuButton";
