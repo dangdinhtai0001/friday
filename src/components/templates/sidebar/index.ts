35f49abb5950f2898132261e1931b4ee97365c9e
dangdinhtai <dangdinhtai0001@gmail.com>
2025-05-19 18:48:04 +0700

    feat: add exports for new sidebar components (SidebarGroup, SidebarMenu, etc.)

--- src/components/templates/sidebar/index.ts
+++ src/components/templates/sidebar/index.ts
@@ -6,3 +6,11 @@ export { default as MCSidebarTrigger } from "./toggle/SidebarTrigger";
 export { default as MCSidebarHeader } from "./container/SidebarHeader";
 export { default as MCSidebarFooter } from "./container/SidebarFooter";
 
+export { default as MCSidebarGroup } from "./content/SidebarGroup";
+export { default as MCSidebarGroupLabel } from "./content/SidebarGroupLabel";
+export { default as MCSidebarGroupAction } from "./content/SidebarGroupAction";
+export { default as MCSidebarGroupContent } from "./content/SidebarGroupContent";
+
+export { default as MCSidebarMenu } from "./menu/SidebarMenu";
+export { default as MCSidebarMenuItem } from "./menu/SidebarMenuItem";
+export { default as MCSidebarMenuButton } from "./menu/SidebarMenuButton";
