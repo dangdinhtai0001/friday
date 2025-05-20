48e7148af6640f25d09b241ae4518b72792af9b3
dangdinhtai <dangdinhtai0001@gmail.com>
2025-05-20 18:33:32 +0700

    feat(breadcrumb): add Breadcrumb components for navigation

---
+++ src/components/molecules/breadcrumb/BreadcrumbList.tsx
@@ -0,0 +1,16 @@
+import { cn } from "@/composables/utils/shadcn";
+
+function BreadcrumbList({ className, ...props }: React.ComponentProps<"ol">) {
+  return (
+    <ol
+      data-slot="breadcrumb-list"
+      className={cn(
+        "text-muted-foreground flex flex-wrap items-center gap-1.5 text-sm break-words sm:gap-2.5",
+        className,
+      )}
+      {...props}
+    />
+  );
+}
+
+export default BreadcrumbList;
---
+++ src/components/molecules/breadcrumb/BreadcrumbSeparator.tsx
@@ -0,0 +1,22 @@
+import { cn } from "@/composables/utils/shadcn";
+import { ChevronRight } from "lucide-react";
+
+function BreadcrumbSeparator({
+  children,
+  className,
+  ...props
+}: React.ComponentProps<"li">) {
+  return (
+    <li
+      data-slot="breadcrumb-separator"
+      role="presentation"
+      aria-hidden="true"
+      className={cn("[&>svg]:size-3.5", className)}
+      {...props}
+    >
+      {children ?? <ChevronRight />}
+    </li>
+  );
+}
+
+export default BreadcrumbSeparator;
