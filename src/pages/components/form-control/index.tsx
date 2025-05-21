b53f1a26a4d91c7a156ed414900dd0bda4b6db26
dangdinhtai <dangdinhtai0001@gmail.com>
2025-05-21 18:44:43 +0700

    feat: add collapsible example in form control page

--- src/pages/components/form-control/index.tsx
+++ src/pages/components/form-control/index.tsx
@@ -28,14 +28,17 @@ import {
   MCDropdownMenuGroup,
   MCDropdownMenuItem,
   MCDropdownMenuLabel,
-  MCDropdownMenuPortal,
   MCDropdownMenuSeparator,
   MCDropdownMenuShortcut,
-  MCDropdownMenuSub,
-  MCDropdownMenuSubContent,
-  MCDropdownMenuSubTrigger,
   MCDropdownMenuTrigger,
 } from "@/components/molecules/dropdown-menu";
+import {
+  MCCollapsibleContent,
+  MCCollapsiable,
+  MCCollapsibleTrigger,
+} from "@/components/atoms/collapsible";
+import { ChevronsUpDown } from "lucide-react";
+import React from "react";
 
 // Định nghĩa các loại mục menu
 type DropdownMenuItemType = {
@@ -278,6 +281,31 @@ const renderDropdownChildren = (
 function Page() {
   return (
     <>
+      <div>Form control component nè</div>
+      <MCCollapsiable className="w-[350px] space-y-2">
+        <div className="flex items-center justify-between space-x-4 px-4">
+          <h4 className="text-sm font-semibold">
+            @peduarte starred 3 repositories
+          </h4>
+          <MCCollapsibleTrigger asChild>
+            <Button className="w-9 p-0">
+              <ChevronsUpDown className="h-4 w-4" />
+              <span className="sr-only">Toggle</span>
+            </Button>
+          </MCCollapsibleTrigger>
+        </div>
+        <div className="rounded-md border px-4 py-3 font-mono text-sm">
+          @radix-ui/primitives
+        </div>
+        <MCCollapsibleContent className="flex flex-col gap-8">
+          <div className="rounded-md border px-4 py-3 font-mono text-sm">
+            @radix-ui/colors
+          </div>
+          <div className="rounded-md border px-4 py-3 font-mono text-sm">
+            @stitches/react
+          </div>
+        </MCCollapsibleContent>
+      </MCCollapsiable>
       <div>Form control component nè</div>
       {/* <MCDropdownMenu>
         <MCDropdownMenuTrigger>
