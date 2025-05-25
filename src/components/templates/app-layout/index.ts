667f675df1b85b0e726e62b15ba7c8c789217d01
dangdinhtai0001 <dangdinhtai0001@gmail.com>
2025-05-26 02:03:23 +0700

    refactor(index): update index exports for new structure

--- src/components/templates/app-layout/index.ts
+++ src/components/templates/app-layout/index.ts
@@ -1,17 +1,4 @@
 export { default as AppLayout } from "./app-layout";
 
-export type { ExpandedSidebarItem } from "./expand/expanded-sidebar-content";
-export type {
-  SidebarSubMenuItem,
-  NestedCollapsibleMenuItem,
-  LinkMenuItem,
-} from "./expand/sidebar-collapsible-item";
-
-export type {
-  DropdownItemType,
-  DropdownLink,
-  DropdownSubmenu,
-  DropdownSeparatorItem,
-  DropdownLabelItem,
-} from "./collapse/sidebar-dropdown-item";
-export type { CollapsedSidebarItem } from "./collapse/collapsed-sidebar-content";
+export * from "./types/common.types";
+export * from "./types/sidebar-menu.types";
