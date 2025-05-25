f2e74880d427eb313d7d2efcb2f5786bd62b8e41
dangdinhtai0001 <dangdinhtai0001@gmail.com>
2025-05-26 00:48:19 +0700

    feat(icons): add new SVG icons for components

--- src/components/molecules/breadcrumb/index.ts
+++
@@ -1,7 +0,0 @@
-export { default as MCBreadcrumb } from "./Breadcrumb";
-export { default as MCBreadcrumbList } from "./BreadcrumbList";
-export { default as MCBreadcrumbItem } from "./BreadcrumbItem";
-export { default as MCBreadcrumbLink } from "./BreadcrumbLink";
-export { default as MCBreadcrumbPage } from "./BreadcrumbPage";
-export { default as MCBreadcrumbSeparator } from "./BreadcrumbSeparator";
-export { default as MCBreadcrumbEllipsis } from "./BreadcrumbEllipsis";
