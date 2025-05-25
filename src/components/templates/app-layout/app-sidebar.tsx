5d9e90103e799cbcfade04cef2b08f9fb8713b02
dangdinhtai0001 <dangdinhtai0001@gmail.com>
2025-05-26 00:53:25 +0700

    refactor(sidebar): Update app sidebar to use new context and structure

--- src/components/templates/app-layout/app-sidebar.tsx
+++ src/components/templates/app-layout/app-sidebar.tsx
@@ -4,8 +4,8 @@ import {
   SidebarFooter,
   SidebarHeader,
 } from "@/components/organisms/sidebar2";
-import CollapsedSidebarContent from "./collapse/CollapsedSidebarContent";
-import ExpandedSidebarContent from "./expand/ExpandedSidebarContent";
+import CollapsedSidebarContent from "./collapse/collapsed-sidebar-content";
+import ExpandedSidebarContent from "./expand/expanded-sidebar-content";
 import { motion, AnimatePresence } from "motion/react";
 import {
   MCAvatar,
@@ -15,6 +15,7 @@ import {
 import { MCSkeleton } from "@/components/atoms/skeleton";
 import logo from "@/assets/images/vite.svg";
 import { Link } from "react-router";
+import { useAppLayoutContext } from "./context/app-layout-context";
 
 // --- Cấu hình animation variants ---
 
@@ -35,14 +36,18 @@ const slideInFromRight = {
 const transition = { duration: 0.3, type: "spring" };
 
 // --- Component AppSidebar ---
-function AppSidebar({
-  isSidebarExpanded,
-  ...props
-}: React.ComponentProps<typeof Sidebar> & { isSidebarExpanded: boolean }) {
+function AppSidebar({ ...props }: React.ComponentProps<typeof Sidebar>) {
+  const {
+    state: { isSidebarExpanded, headerHeight },
+  } = useAppLayoutContext();
+
   return (
     <Sidebar {...props} className="bg-background-2">
       <Link to="/">
-        <SidebarHeader className="typography-regular-24 rounded-8 flex h-[68px] w-full cursor-pointer items-center justify-start gap-12 px-8">
+        <SidebarHeader
+          className="typography-regular-24 rounded-8 flex w-full cursor-pointer items-center justify-start gap-12 px-8"
+          style={{ height: headerHeight }}
+        >
           <MCAvatar className="size-32">
             <MCAvatarImage src={logo} />
             <MCAvatarFallback>
@@ -57,7 +62,7 @@ function AppSidebar({
         </SidebarHeader>
       </Link>
 
-      <SidebarContent className="relative">
+      <SidebarContent className="relative overflow-x-hidden">
         <AnimatePresence initial={false} mode="popLayout">
           {isSidebarExpanded ? (
             <motion.div
@@ -87,7 +92,7 @@ function AppSidebar({
         </AnimatePresence>
       </SidebarContent>
 
-      <SidebarFooter className="flex items-center justify-center ">
+      <SidebarFooter className="flex items-center justify-center">
         {/* <span className="typography-regular-12 text-black-100">0.0.0</span> */}
       </SidebarFooter>
     </Sidebar>
