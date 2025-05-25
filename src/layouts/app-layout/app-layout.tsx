3a2fab53469e275a3ee0f89b2818bcf0a22b435d
dangdinhtai0001 <dangdinhtai0001@gmail.com>
2025-05-26 02:03:14 +0700

    refactor(layout): simplify layout data conversion logic

--- src/layouts/app-layout/app-layout.tsx
+++ src/layouts/app-layout/app-layout.tsx
@@ -1,139 +1,15 @@
 import { AppLayout as AppLayoutTemplate } from "@/components/templates/app-layout";
 import {
-  AppLayoutMenuItem,
-  convertAppLayoutItemToCollapsedItem,
-  convertAppLayoutItemToExpandedItem,
-  createUrlToDisplayNameMap,
-} from "./helpers";
-import {
-  CollapsedSidebarItem as CollapsedSidebarItemType,
-  ExpandedSidebarItem as ExpandedSidebarItemType,
-} from "@/components/templates/app-layout";
-import { convertUnifiedToCollapsedSidebarItems, convertUnifiedToExpandedSidebarItems, initialUnifiedRoutes } from "@/config";
+  convertUnifiedToCollapsedSidebarItems,
+  convertUnifiedToExpandedSidebarItems,
+  initialUnifiedRoutes,
+} from "@/config";
 
-const platform1DropdownItemsUnified: AppLayoutMenuItem[] = [
-  // Cập nhật tên interface
-  {
-    id: "overview-platform1",
-    type: "link",
-    text: "Overview",
-    link: "/overview",
-    icon: "dashboard",
-  },
-  {
-    id: "projects-platform1",
-    type: "link",
-    text: "Projects",
-    link: "#",
-    icon: "folder",
-  },
-  {
-    id: "settings-platform1",
-    type: "submenu",
-    text: "Settings",
-    icon: "settings",
-    submenuItems: [
-      {
-        id: "account-settings",
-        type: "link",
-        text: "Account",
-        link: "#",
-        icon: "user",
-      },
-      {
-        id: "privacy-settings",
-        type: "link",
-        text: "Privacy",
-        link: "#",
-        icon: "lock-square",
-      },
-      {
-        id: "notifications-settings",
-        type: "submenu",
-        text: "Notifications",
-        icon: "bell",
-        submenuItems: [
-          {
-            id: "email-notifications",
-            type: "link",
-            text: "Email settings",
-            link: "#",
-            icon: "mail",
-          },
-          {
-            id: "sms-notifications",
-            type: "link",
-            text: "SMS settings",
-            link: "#",
-            icon: "message-circle",
-          },
-          {
-            id: "advanced-notifications",
-            type: "submenu",
-            text: "Advanced",
-            icon: "command",
-            submenuItems: [
-              {
-                id: "api-keys-advanced",
-                type: "link",
-                text: "API Keys",
-                link: "#",
-                icon: "key",
-              },
-              {
-                id: "webhooks-advanced",
-                type: "link",
-                text: "Webhooks",
-                link: "#",
-                icon: "webhook",
-              },
-            ],
-          },
-        ],
-      },
-    ],
-  },
-];
-
-const menuItemsUnified: AppLayoutMenuItem[] = [
-  // Cập nhật tên interface
-  {
-    id: "tooltip-id",
-    type: "submenu",
-    icon: "id",
-    text: "Tooltip for ID",
-    submenuItems: platform1DropdownItemsUnified,
-  },
-  {
-    id: "tooltip-rocket",
-    type: "submenu",
-    icon: "rocket",
-    text: "Tooltip for Rocket",
-    submenuItems: platform1DropdownItemsUnified,
-  },
-];
-
-// Create the URL to display name map
-const urlToDisplayNameMap = createUrlToDisplayNameMap(menuItemsUnified);
-
-// Gọi hàm convert để tạo ra collapsedSidebarItemsData
-const generatedCollapsedSidebarItemsData: CollapsedSidebarItemType[] =
-  menuItemsUnified
-    .map(convertAppLayoutItemToCollapsedItem)
-    .filter((item) => item !== null) as CollapsedSidebarItemType[];
-
-const generatedExpandedSidebarItemsData: ExpandedSidebarItemType[] =
-  menuItemsUnified
-    .map(convertAppLayoutItemToExpandedItem)
-    .filter((item) => item !== null) as ExpandedSidebarItemType[];
-
+const expandedSidebarItems =
+  convertUnifiedToExpandedSidebarItems(initialUnifiedRoutes);
+const collapsedSidebarItems =
+  convertUnifiedToCollapsedSidebarItems(initialUnifiedRoutes);
 function AppLayout() {
-
-  const expandedSidebarItems = convertUnifiedToExpandedSidebarItems(initialUnifiedRoutes);
-  const collapsedSidebarItems = convertUnifiedToCollapsedSidebarItems(initialUnifiedRoutes);
-
-  console.log(collapsedSidebarItems);
-
   return (
     <>
       <AppLayoutTemplate
@@ -141,8 +17,6 @@ function AppLayout() {
         defaultSidebarExpanedWidth={"212px"}
         defaultHeaderHeight={"68px"}
         defaultSidebarCollapsedWidth={"64px"}
-        // collapsedSidebarItemsData={generatedCollapsedSidebarItemsData}
-        // expandedSidebarItemsData={generatedExpandedSidebarItemsData}
         expandedSidebarItemsData={expandedSidebarItems}
         collapsedSidebarItemsData={collapsedSidebarItems}
       ></AppLayoutTemplate>
