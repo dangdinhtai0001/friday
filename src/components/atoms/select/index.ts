31b61c08b0eb8e536bfce181483970fcd7ab8b33
dangdinhtai0001 <dangdinhtai0001@gmail.com>
2025-05-27 01:54:46 +0700

    feat: create radix-ui react-select component hierarchy

---
+++ src/components/atoms/select/index.ts
@@ -0,0 +1,16 @@
+export {
+  Select as MCSelect,
+  SelectGroup as MCSelectGroup,
+  SelectValue as MCSelectValue,
+} from "./select-root";
+export { SelectTrigger as MCSelectTrigger } from "./select-trigger";
+export {
+  SelectContent as MCSelectContent,
+  SelectScrollUpButton as MCSelectScrollUpButton,
+  SelectScrollDownButton as MCSelectScrollDownButton,
+} from "./select-content";
+export {
+  SelectItem as MCSelectItem,
+  SelectLabel as MCSelectLabel,
+  SelectSeparator as MCSelectSeparator,
+} from "./select-items";
---
+++ src/components/atoms/select/select-root.tsx
@@ -0,0 +1,22 @@
+import * as React from "react";
+import * as SelectPrimitive from "@radix-ui/react-select";
+
+function Select({
+  ...props
+}: React.ComponentProps<typeof SelectPrimitive.Root>) {
+  return <SelectPrimitive.Root data-slot="select" {...props} />;
+}
+
+function SelectGroup({
+  ...props
+}: React.ComponentProps<typeof SelectPrimitive.Group>) {
+  return <SelectPrimitive.Group data-slot="select-group" {...props} />;
+}
+
+function SelectValue({
+  ...props
+}: React.ComponentProps<typeof SelectPrimitive.Value>) {
+  return <SelectPrimitive.Value data-slot="select-value" {...props} />;
+}
+
+export { Select, SelectGroup, SelectValue };
