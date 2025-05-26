7fcd60f6c8202ff0aa0bbf82f7d3d4befc9ba457
dangdinhtai0001 <dangdinhtai0001@gmail.com>
2025-05-27 01:55:07 +0700

    feat: integrate select and theme switcher in system status page

--- src/pages/system-status/index.tsx
+++ src/pages/system-status/index.tsx
@@ -1,9 +1,37 @@
+import {
+  MCSelect,
+  MCSelectTrigger,
+  MCSelectValue,
+  MCSelectContent,
+  MCSelectItem,
+} from "@/components/atoms/select";
+import { ECThemeSwitcher } from "@/components/molecules/theme-switcher";
+import React from "react";
+
 function Page() {
-    return (
-        <div>
-            <h1>System Status</h1>
-        </div>
-    );
+  const [value, setValue] = React.useState("france");
+
+  const handleValueChange = (value: string) => {
+    setValue(value);
+  }
+  return (
+    <div>
+      <h1>System Status</h1>
+
+      <MCSelect value={value} onValueChange={handleValueChange}>
+        <MCSelectTrigger className="w-[180px]">
+          <MCSelectValue placeholder="Theme" />
+        </MCSelectTrigger>
+        <MCSelectContent>
+          <MCSelectItem value="light">Light</MCSelectItem>
+          <MCSelectItem value="dark">Dark</MCSelectItem>
+          <MCSelectItem value="system">System</MCSelectItem>
+        </MCSelectContent>
+      </MCSelect>
+
+      <ECThemeSwitcher />
+    </div>
+  );
 }
 
 export default Page;
