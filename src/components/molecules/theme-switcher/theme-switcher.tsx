15f8c381a667a26b688289cc7d96b68451d7c4ee
dangdinhtai <dangdinhtai0001@gmail.com>
2025-05-27 18:51:50 +0700

    refactor(theme-switcher): convert to dropdown menu UI

--- src/components/molecules/theme-switcher/theme-switcher.tsx
+++ src/components/molecules/theme-switcher/theme-switcher.tsx
@@ -1,24 +1,70 @@
-import { useThemeStore } from "@/store";
+import {
+  MCDropdownMenu,
+  MCDropdownMenuContent,
+  MCDropdownMenuGroup,
+  MCDropdownMenuItem,
+  MCDropdownMenuTrigger,
+} from "../dropdown-menu";
+import { MCButton } from "@/components/atoms/button";
+import { IconLoader } from "@/components/atoms/icon-loader";
+import { useTheme } from "@/composables/hooks/use-theme";
+import React from "react";
+import { find as _find } from "lodash-es";
+import { Theme } from "@/store";
+
+type ThemeOption = {
+  value: Theme;
+  text: string;
+  icon: string;
+};
+
+const options: ThemeOption[] = [
+  { value: "pastel-light", text: "pastel light", icon: "sun" },
+  { value: "pastel-dark", text: "pastel dark", icon: "moon" },
+];
 
 function ThemeSwitcher() {
-  const theme = useThemeStore((state) => state.theme);
-  const setTheme = useThemeStore((state) => state.setTheme);
-  const toggleTheme = useThemeStore((state) => state.toggleTheme);
+  const { theme, setTheme } = useTheme();
 
-  const handleChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
-    setTheme(event.target.value as 'pastel-light' | 'pastel-dark');
+  const [currentOption, setCurrentOption] = React.useState<ThemeOption>(
+    _find(options, { value: theme }) || options[0],
+  );
+  const handleSelect = (option: ThemeOption) => {
+    setCurrentOption(option);
+    setTheme(option.value);
   };
 
   return (
-    <div>
-      <h1>Theme Switcher</h1>
-      <div>{theme}</div>
-      <button onClick={toggleTheme}>Toggle Theme</button>
-      <select onChange={handleChange} value={theme}>
-        <option value="pastel-light">Pastel Light</option>
-        <option value="pastel-dark">Pastel Dark</option>
-      </select>
-    </div>
+    <MCDropdownMenu>
+      <MCDropdownMenuTrigger asChild>
+        <MCButton
+          leftIcon={
+            <IconLoader name={currentOption.icon} className="size-20" />
+          }
+        />
+      </MCDropdownMenuTrigger>
+
+      <MCDropdownMenuContent
+        className="typography-regular-12 text-black-100 w-[8rem]"
+        side="bottom"
+      >
+        <MCDropdownMenuGroup>
+          {options.map((option, index) => (
+            <MCDropdownMenuItem
+              key={index}
+              onSelect={() => {
+                handleSelect(option);
+              }}
+            >
+              <div className="rounded-8 flex items-center justify-start gap-4">
+                <IconLoader name={option.icon} />
+                <div>{option.text}</div>
+              </div>
+            </MCDropdownMenuItem>
+          ))}
+        </MCDropdownMenuGroup>
+      </MCDropdownMenuContent>
+    </MCDropdownMenu>
   );
 }
 
