be3dc54921fa401333cc93af707005e2b75dd88f
dangdinhtai0001 <dangdinhtai0001@gmail.com>
2025-05-28 01:42:10 +0700

    feat(i18n): initialize i18next in App

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import "@/assets/styles/main.css";
 import { RouterProvider } from "react-router";
 import { router } from "@/router";
+import './i18n';
 
 function App() {
   return <RouterProvider router={router} />;
