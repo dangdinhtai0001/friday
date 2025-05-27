57d4848c2ea49f22b9d4ec79bcb165a8636148eb
dangdinhtai <dangdinhtai0001@gmail.com>
2025-05-27 18:52:11 +0700

    refactor(system-status): remove redundant theme switcher

--- src/pages/system-status/index.tsx
+++ src/pages/system-status/index.tsx
@@ -1,35 +1,7 @@
-import {
-  MCSelect,
-  MCSelectTrigger,
-  MCSelectValue,
-  MCSelectContent,
-  MCSelectItem,
-} from "@/components/atoms/select";
-import { ECThemeSwitcher } from "@/components/molecules/theme-switcher";
-import React from "react";
-
 function Page() {
-  const [value, setValue] = React.useState("france");
-
-  const handleValueChange = (value: string) => {
-    setValue(value);
-  }
   return (
     <div>
       <h1>System Status</h1>
-
-      <MCSelect value={value} onValueChange={handleValueChange}>
-        <MCSelectTrigger className="w-[180px]">
-          <MCSelectValue placeholder="Theme" />
-        </MCSelectTrigger>
-        <MCSelectContent>
-          <MCSelectItem value="light">Light</MCSelectItem>
-          <MCSelectItem value="dark">Dark</MCSelectItem>
-          <MCSelectItem value="system">System</MCSelectItem>
-        </MCSelectContent>
-      </MCSelect>
-
-      <ECThemeSwitcher />
     </div>
   );
 }
