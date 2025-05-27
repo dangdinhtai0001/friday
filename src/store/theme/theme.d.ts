ec69358582a63152bcdf96cabb16288f6579b259
dangdinhtai0001 <dangdinhtai0001@gmail.com>
2025-05-27 11:58:50 +0700

    feat: define ThemeStore interface for type safety

--- src/store/theme/theme.d.ts
+++ src/store/theme/theme.d.ts
@@ -1,1 +1,8 @@
-export type Theme = 'pastel-light' | 'pastel-dark';
+// src/store/theme.d.ts
+export type Theme = "pastel-light" | "pastel-dark";
+
+export interface ThemeStore {
+  theme: Theme;
+  setTheme: (newTheme: Theme) => void;
+  toggleTheme: () => void;
+}
