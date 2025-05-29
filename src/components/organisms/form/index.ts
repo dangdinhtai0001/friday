55f52bedd47b01705392734358df908b4e5c16d8
dangdinhtai <dangdinhtai0001@gmail.com>
2025-05-29 18:45:20 +0700

    chore(form): update form component exports

--- src/components/organisms/form/index.ts
+++ src/components/organisms/form/index.ts
@@ -1,2 +1,6 @@
-export {default as ECFormContainerProvider} from './context/form-container/form-container-provider'
-export {default as ECFormContainer} from './form-container'
+export { default as ECFormContainerProvider } from './context/form-container/form-container-provider';
+export { default as ECFormContainer } from './form-container';
+
+export { default as ECFormField } from './form-field';
+export { default as ECFormFieldLabel } from './field/form-field-label';
+export { default as ECFormFieldControl } from './field/form-field-control';
