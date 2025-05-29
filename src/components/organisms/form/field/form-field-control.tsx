8f0fb22ad992b4e50ff77f69658227550d66b335
dangdinhtai0001 <dangdinhtai0001@gmail.com>
2025-05-30 02:41:49 +0700

    feat(form): update form control and label to use grid layout positioning

--- src/components/organisms/form/field/form-field-control.tsx
+++ src/components/organisms/form/field/form-field-control.tsx
@@ -1,15 +1,23 @@
 import { Slot } from '@radix-ui/react-slot';
 import { useFormFieldContext } from '../context/form-field/form-field-context';
 import { useController } from 'react-hook-form';
+import { ECGridItem } from '@/components/atoms/grid-layout';
+import { formFieldLayoutMap } from './helper';
 
 function FormFieldControl({ ...props }: React.ComponentProps<typeof Slot>) {
   const {
-    state: { controlId, fieldName },
+    state: { controlId, fieldName, labelPlacement },
   } = useFormFieldContext();
 
   const { field } = useController({ name: fieldName });
 
-  return <Slot data-slot="form-control" id={controlId} {...props} {...field} />;
+  const { x, y } = formFieldLayoutMap[labelPlacement || 'top'].control;
+
+  return (
+    <ECGridItem x={x} y={y} className="">
+      <Slot data-slot="form-control" id={controlId} {...props} {...field} />
+    </ECGridItem>
+  );
 }
 
 export default FormFieldControl;
--- src/components/organisms/form/field/form-field-label.tsx
+++ src/components/organisms/form/field/form-field-label.tsx
@@ -2,6 +2,7 @@ import { MCLabel } from '@/components/atoms/label';
 import { cn } from '@/composables/utils/shadcn';
 import * as LabelPrimitive from '@radix-ui/react-label';
 import { useFormFieldContext } from '../context/form-field/form-field-context';
+import { ECGridItem } from '@/components/atoms/grid-layout';
 
 function FormFieldLabel({
   className,
@@ -12,13 +13,15 @@ function FormFieldLabel({
   } = useFormFieldContext();
 
   return (
-    <MCLabel
-      data-slot="form-label"
-      //   data-error={!!error}
-      className={cn('data-[error=true]:text-destructive', className)}
-      htmlFor={controlId}
-      {...props}
-    />
+    <ECGridItem x={1} y={1} className="">
+      <MCLabel
+        data-slot="form-label"
+        //   data-error={!!error}
+        className={cn('typography-regular-12 text-black-40 ', className)}
+        htmlFor={controlId}
+        {...props}
+      />
+    </ECGridItem>
   );
 }
 
