bde92d9b82b9c3a9cbea89ebfd0a9f376e4eccf5
dangdinhtai <dangdinhtai0001@gmail.com>
2025-05-30 18:40:34 +0700

    refactor(grid-layout): update exports for consistency

--- src/components/atoms/grid-layout/index.ts
+++ src/components/atoms/grid-layout/index.ts
@@ -1,4 +1,7 @@
-export { default as ECGridLayout } from "./grid-layout";
-export { default as ECGridItem } from "./grid-item";
+export { default as ECGridLayout } from './grid-layout';
+export { default as ECGridItem } from './grid-item';
 
-export * from './types.d'
+export type {
+  GridItemProps as ECGridItemProps,
+  GridLayoutProps as ECGridLayoutProps,
+} from './types.d';
