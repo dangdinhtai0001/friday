a47e9fd737bd36c3138cf07c56c98c86f8fe26c0
dangdinhtai <dangdinhtai0001@gmail.com>
2025-05-30 18:40:39 +0700

    feat(form): add form container state management

--- src/components/organisms/form/context/context.types.ts
+++ src/components/organisms/form/context/context.types.ts
@@ -1,8 +1,30 @@
 /* eslint-disable @typescript-eslint/no-empty-object-type */
-export type FormContainerState = {
-  formId: string;
+export type FormStatus = 'idle' | 'loading';
+export type FieldControlState = {
+  message?: string;
+  messageType?: 'error' | 'warning' | 'info' | 'success';
+  disabled?: boolean;
+  readOnly?: boolean;
 };
-export type FormContainerActions = {};
+
+export type FormContainerState = {
+  id: string;
+  status: FormStatus;
+  fieldStates: { [fieldName: string]: FieldControlState };
+};
+
+export type FormContainerActions = {
+  setStatus: (status: FormStatus) => void;
+  setFieldState: (
+    fieldName: string,
+    newState: Partial<FieldControlState>,
+  ) => void;
+  setFieldMessage: (
+    fieldName: string,
+    message?: string, // Nội dung tin nhắn (có thể là undefined để xóa)
+    messageType?: FieldControlState['messageType'], // Loại tin nhắn (error, warning, info, success)
+  ) => void;
+};
 
 export type FormContainerContextValue = {
   state: FormContainerState;
--- src/components/organisms/form/context/form-container/form-container-provider.tsx
+++ src/components/organisms/form/context/form-container/form-container-provider.tsx
@@ -1,25 +1,54 @@
 import React from 'react';
-import { v4 as uuidv4 } from 'uuid';
 import {
+  FieldControlState,
   FormContainerActions,
   FormContainerContextValue,
   FormContainerState,
+  FormStatus,
   FromContainerProviderProps,
 } from '../context.types';
 import { FormContainerContext } from './form-container-context';
 
-const initialState: FormContainerState = {
-  formId: uuidv4(),
-};
-
 function FormContainerProvider({
   children,
 }: React.PropsWithChildren<FromContainerProviderProps>) {
-  const [state] = React.useState<FormContainerState>({
-    ...initialState,
+  const id = React.useId();
+
+  const [state, setState] = React.useState<FormContainerState>({
+    id: `__form-${id}`,
+    status: 'idle',
+    fieldStates: {},
   });
 
-  const actions: FormContainerActions = {};
+  const setFieldState = (
+    fieldName: string,
+    newState: Partial<FieldControlState>,
+  ) => {
+    setState((prevState) => ({
+      ...prevState,
+      fieldStates: {
+        ...prevState.fieldStates,
+        [fieldName]: {
+          ...prevState.fieldStates[fieldName],
+          ...newState,
+        },
+      },
+    }));
+  };
+
+  const actions: FormContainerActions = {
+    setStatus: (status: FormStatus) => {
+      setState((prevState) => ({ ...prevState, status }));
+    },
+    setFieldState: setFieldState,
+    setFieldMessage: (
+      fieldName: string,
+      message?: string,
+      messageType?: FieldControlState['messageType'],
+    ) => {
+      setFieldState(fieldName, { message, messageType });
+    },
+  };
 
   return (
     <FormContainerContext.Provider
