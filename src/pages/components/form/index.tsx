a20d7f37f0bc440423262ca44f226b109637e8ff
dangdinhtai <dangdinhtai0001@gmail.com>
2025-05-30 18:41:00 +0700

    feat(demo): update form demo page with new features

--- src/pages/components/form/index.tsx
+++ src/pages/components/form/index.tsx
@@ -1,3 +1,4 @@
+import { MCButton } from '@/components/atoms/button';
 import MCTextField from '@/components/atoms/text-field/MCTextField';
 import {
   ECFormContainer,
@@ -6,21 +7,52 @@ import {
   ECFormFieldControl,
   ECFormFieldDescription,
   ECFormFieldLabel,
+  useFormController,
 } from '@/components/organisms/form';
+import { FormContainerState } from '@/components/organisms/form/context/context.types';
+import { ValidateResponse } from '@/components/organisms/form/form-container';
 
 export interface ProductInput {
-  productName: string;
-  price: number;
-  category: "Electronics" | "Books" | "Clothing" | "Home & Kitchen" | "Other";
-  isInStock: boolean;
+  productName?: string;
+  price?: number;
+  category?: 'Electronics' | 'Books' | 'Clothing' | 'Home & Kitchen' | 'Other';
+  isInStock?: boolean;
 }
 
+const validateFunction = async (
+  data: ProductInput,
+): Promise<ValidateResponse<ProductInput>> => {
+  const errors: Record<string, { message: string }> = {};
+
+  if (data.productName === '123') {
+    errors.productName = { message: 'Có lỗi nè' };
+  }
+
+  return {
+    values: Object.keys(errors).length > 0 ? {} : data,
+    errors,
+  };
+};
+
 function Page() {
+  const { formRef, resetForm, submitForm, validateForm, getFieldsError } =
+    useFormController();
   return (
-    <div>
+    <>
       <ECFormContainerProvider>
-        <ECFormContainer cols={2}>
-          <ECFormField name='productName'>
+        <ECFormContainer
+          cols={4}
+          className="border"
+          onReady={(state: FormContainerState) => {
+            console.log(`form: ${state.id} ready`);
+          }}
+          onSubmit={(data: ProductInput) => {
+            console.log(data);
+          }}
+          validateFunction={validateFunction}
+          ref={formRef}
+        >
+          <ECFormField name="productName" width={2} height={2}>
             <ECFormFieldLabel>Product Name</ECFormFieldLabel>
             <ECFormFieldControl>
               <MCTextField />
@@ -29,14 +61,48 @@ function Page() {
               <span>hehehe</span>
             </ECFormFieldDescription>
           </ECFormField>
-          <ECFormField name='price'>
+          <ECFormField name="price">
             <ECFormFieldLabel>Price</ECFormFieldLabel>
           </ECFormField>
-          <ECFormField name='category'>3</ECFormField>
-          <ECFormField name='isInStock'>4</ECFormField>
+          <ECFormField name="category">
+          <ECFormFieldLabel>Category</ECFormFieldLabel>
+          </ECFormField>
+          <ECFormField name="isInStock">
+          <ECFormFieldLabel>In stock</ECFormFieldLabel>
+          </ECFormField>
         </ECFormContainer>
       </ECFormContainerProvider>
-    </div>
+      <div className="flex items-center gap-4">
+        <MCButton
+          onClick={() => {
+            submitForm();
+          }}
+        >
+          Submit
+        </MCButton>
+        <MCButton
+          onClick={() => {
+            resetForm();
+          }}
+        >
+          Reset
+        </MCButton>
+        <MCButton
+          onClick={() => {
+            validateForm();
+          }}
+        >
+          Validate
+        </MCButton>
+        <MCButton
+          onClick={() => {
+            getFieldsError();
+          }}
+        >
+          get field err
+        </MCButton>
+      </div>
+    </>
   );
 }
 
