c01cfecddfefd07eb79f173d9500fe0e1e784967
dangdinhtai <dangdinhtai0001@gmail.com>
2025-06-05 18:56:05 +0700

    refactor: enhance i18n types with new namespace

--- src/i18n/types.ts
+++ src/i18n/types.ts
@@ -1,14 +1,17 @@
-import "i18next";
+import 'i18next';
 
-import common from "@/../../public/locales/en/common.json";
-import themeColor from "@/../../public/locales/en/pages/theme-color.json";
+import common from '@/../../public/locales/en/common.json';
+import themeColor from '@/../../public/locales/en/pages/theme-color.json';
 
-declare module "i18next" {
+import componentSelect from '@/../../public/locales/en/component/select.json';
+
+declare module 'i18next' {
   interface CustomTypeOptions {
-    defaultNS: "common";
+    defaultNS: 'common';
     resources: {
       common: typeof common;
-      "pages/theme-color": typeof themeColor;
+      'pages/theme-color': typeof themeColor;
+      'component/select': typeof componentSelect;
     };
   }
 }
