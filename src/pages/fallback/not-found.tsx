e67b41dd9eed53c6faa3e83c3bd25c5c7abead9d
dangdinhtai0001 <dangdinhtai0001@gmail.com>
2025-06-06 12:07:05 +0700

    feat: add custom 404 error page with LottieLoader animation

---
+++ src/pages/fallback/not-found.tsx
@@ -0,0 +1,35 @@
+import animationData from '@/assets/lotties/dog-swimming-outlines.json';
+import { Link } from 'react-router';
+import { ECButton } from '@/components/atoms/button';
+import { ECLottieLoader } from '@/components/atoms/lottie-loader';
+
+function Page() {
+  return (
+    <div className="flex h-screen w-full items-center justify-center gap-12">
+      <div className="flex flex-col items-center gap-8">
+        <div className="typography-semibold-48 text-black-100">
+          Ối! Có vẻ bạn đã đi lạc rồi
+        </div>
+        <div className="typography-regular-20 text-black-100/80">
+          Trang bạn đang tìm kiếm có thể đã được chuyển đi hoặc không còn tồn
+          tại nữa.
+        </div>
+        <div className="h-16" />
+        <Link to="/" className="back-home-button">
+          <ECButton variant="filled" size="medium">
+            Về trang chủ
+          </ECButton>
+        </Link>
+      </div>
+      <div>
+        <ECLottieLoader
+          options={{ animationData: animationData }}
+          height={600}
+          width={600}
+        />
+      </div>
+    </div>
+  );
+}
+
+export default Page;
