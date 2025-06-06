82688bb5280686b7f9bd735696657c38db0fb2f5
dangdinhtai0001 <dangdinhtai0001@gmail.com>
2025-06-06 12:07:00 +0700

    feat: add PageLoader organism component for full-page loading

---
+++ src/components/organisms/page-loader/page-loader.tsx
@@ -0,0 +1,16 @@
+import { ECLottieLoader } from "@/components/atoms/lottie-loader";
+import animationData from "@/assets/lotties/plane-animation.json";
+
+function PageLoader() {
+  return (
+    <div className="flex h-screen w-full items-center justify-center">
+      <ECLottieLoader
+        options={{ animationData: animationData }}
+        height={600}
+        width={600}
+      />
+    </div>
+  );
+}
+
+export default PageLoader;
