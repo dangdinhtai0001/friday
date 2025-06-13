10205980267c45af0cefaf7c76ce1c7d6cc7e89c
dangdinhtai <dangdinhtai0001@gmail.com>
2025-06-13 15:03:10 +0700

    feat(data-grid): add constants for default dimensions and events

---
+++ src/components/organisms/enhanced-data-grid/constants.ts
@@ -0,0 +1,9 @@
+export const DEFAULT_ROW_HEIGHT = 50;
+export const DEFAULT_COLUMN_WIDTH = 150;
+export const DEFAULT_COLUMN_MIN_WIDTH = 100;
+
+export const EVENT_NAMESPACE = "data-grid";
+export const EVENT_NAME = {
+  OPEN_PANEL_CHOOSE_COLUMN: "open-panel-choose-column",
+  CLOSE_PANEL_CHOOSE_COLUMN: "close-panel-choose-column",
+};
