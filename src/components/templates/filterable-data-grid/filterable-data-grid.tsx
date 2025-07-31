c44ddc419933e7bff1b8c149a61038a0b4868bb7
dangdinhtai <dangdinhtai0001@gmail.com>
2025-07-31 10:41:50 +0700

    feat(components): add basic FilterableDataGrid component

---
+++ src/components/templates/filterable-data-grid/filterable-data-grid.tsx
@@ -0,0 +1,10 @@
+function FilterableDataGrid() {
+  return (
+    <>
+      <div className="border-black-10 rounded-8 border p-8">
+      </div>
+    </>
+  );
+}
+
+export default FilterableDataGrid;
---
+++ src/components/templates/filterable-data-grid/index.ts
@@ -0,0 +1,1 @@
+export { default as FilterableDataGrid } from './filterable-data-grid';
