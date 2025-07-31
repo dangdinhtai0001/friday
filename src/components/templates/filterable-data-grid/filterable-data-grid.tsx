ff2c874bf1c8a52c0bb92f48fac1abc342c62d0d
dangdinhtai <dangdinhtai0001@gmail.com>
2025-07-31 11:13:40 +0700

    feat: add logic to filterable data grid

--- src/components/templates/filterable-data-grid/filterable-data-grid.tsx
+++ src/components/templates/filterable-data-grid/filterable-data-grid.tsx
@@ -1,7 +1,42 @@
-function FilterableDataGrid() {
+import { ECButton } from '@/components/atoms/button';
+import { useFormController } from '@/components/organisms/form';
+import { FormRef } from '@/components/organisms/form/form-container';
+import { PropsWithChildren, RefObject } from 'react';
+import { FieldValues } from 'react-hook-form';
+
+export interface FilterableDataGridProps {
+  filterContainer?:
+    | React.ReactNode
+    | ((ref: RefObject<FormRef<FieldValues> | null>) => React.ReactNode);
+}
+
+function FilterableDataGrid({
+  filterContainer,
+}: PropsWithChildren<FilterableDataGridProps>) {
+  const { formRef, resetForm, submitForm, validateForm, getFieldsError, getFormValue } =
+    useFormController();
+
+  const renderFilterContainer = () => {
+    if (filterContainer) {
+      return typeof filterContainer === 'function'
+        ? filterContainer(formRef)
+        : filterContainer;
+    }
+
+    return <></>;
+  };
+
   return (
     <>
       <div className="border-black-10 rounded-8 border p-8">
+        {renderFilterContainer()}
+        <ECButton
+          onClick={() => {
+            console.log(getFormValue());
+          }}
+        >
+          Submit
+        </ECButton>
       </div>
     </>
   );
--- src/pages/customer/index.tsx
+++ src/pages/customer/index.tsx
@@ -1,11 +1,61 @@
+import {
+  ECBaseOption,
+  ECControlledSelect,
+} from '@/components/molecules/controlled-select';
+import { BaseOption } from '@/components/molecules/controlled-select/types';
+import {
+  ECFormContainerProvider,
+  ECFormContainer,
+  ECFormField,
+  ECFormFieldLabel,
+  ECFormFieldControl,
+} from '@/components/organisms/form';
 import { FilterableDataGrid } from '@/components/templates/filterable-data-grid';
 
 function Page() {
+
+
+  const renderFilterContainer = () => {
+    return (
+      <>
+        <ECFormContainerProvider>
+          <ECFormContainer cols={3} rows={1}>
+            <ECFormField name="status" required width={1}>
+              <ECFormFieldLabel>Trạng thái</ECFormFieldLabel>
+              <ECFormFieldControl>
+                <ECControlledSelect
+                  placeholder="Tùy chọn giao hàng"
+                  options={fetchCustomerStatus}
+                  getOptionValue={(option) => option.value}
+                  renderLabel={(option: BaseOption) => (
+                    <div className="w-full">{option.label}</div>
+                  )}
+                />
+              </ECFormFieldControl>
+            </ECFormField>
+          </ECFormContainer>
+        </ECFormContainerProvider>
+      </>
+    );
+  };
   return (
     <>
-      <FilterableDataGrid></FilterableDataGrid>
+      <FilterableDataGrid
+        filterContainer={renderFilterContainer}
+      ></FilterableDataGrid>
     </>
   );
 }
+
+const fetchCustomerStatus = async (): Promise<ECBaseOption[]> => {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve([
+        { value: 'ACTIVE', label: 'Active' },
+        { value: 'INACTIVE', label: 'Inactive' },
+      ]);
+    }, 500); // Giả lập độ trễ
+  });
+};
 
 export default Page;
